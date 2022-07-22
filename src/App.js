8e0a6a55c859dd16361c325b9b8aaeea3f4f2d8d
Ashutosh Vishwakarma <ashvish183@gmail.com>
2022-07-23 01:48:59 +0530

    added grid

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Board from './components/Board'
+
+const App=()=>{
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </>
+  );
+}
+
+export default App;
